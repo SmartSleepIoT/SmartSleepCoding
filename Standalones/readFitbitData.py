03809f19582a8b83ec6bcbd918ccc6861d98d250
Mira Stroie <miramaria27@yahoo.com>
2022-01-06 12:08:53 +0200

    Update fitbit data.

--- Standalones/readFitbitData.py
+++ Standalones/readFitbitData.py
@@ -2,7 +2,7 @@ import requests, csv
 from SmartSleep import secrets
 
 header = {'Authorization': f'Bearer {secrets.access_token}'}
-response = requests.get(f'https://api.fitbit.com/1/user/{secrets.user_id}/activities/heart/date/2021-10-11/2021-10-12/1min/time/23:00/08:00.json', headers=header).json()
+response = requests.get(f'https://api.fitbit.com/1/user/{secrets.user_id}/activities/heart/date/2021-10-13/2021-10-13/1min/time/00:20/08:06.json', headers=header).json()
 response = response['activities-heart-intraday']['dataset']
 
 f = open('SensorsData/Heartrate.csv', 'w')
