8bc32092037a6b00d616c70da4f81911ef5ea63e
Mira Stroie <miramaria27@yahoo.com>
2022-01-15 18:18:34 +0200

    New request to get heartrate data.

--- start-app.bat
+++ start-app.bat
@@ -2,7 +2,8 @@ cd SmartSleep
 flask init-db
 START %COMSPEC% /k flask run
 cd ..\\Standalones
-START %COMSPEC% /k python subMQTT.py
-START %COMSPEC% /k python snoring.py
-START %COMSPEC% /k python readSensorData.py
+START %COMSPEC% /k py subMQTT.py
+START %COMSPEC% /k py snoring.py
+START %COMSPEC% /k py sleepStages.py
+START %COMSPEC% /k py readSensorData.py
 EXIT
